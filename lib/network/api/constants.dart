cb77fe0ac3f3c3a3c0c08b9a04ab610c32404a71
ayomi <ayomilotunde02@gmail.com>
2024-11-12 13:05:31 +0100

    update

--- lib/network/api/constants.dart
+++ lib/network/api/constants.dart
@@ -5,74 +5,21 @@ class ApiConstants {
 
   static String login = '/user/login';
   static String createAccount = '/user/createAccount';
-  static String profile = '/user/get-user-detail';
-  static String updateProfile = '/user/update-user-details';
-  static String updatePassword = '/user/update-password';
-  static String updateToken = '/account/update-token';
-  static String verifyAccount = '/user/verify-account';
-  static String resendOTP = '/user/resend-otp';
-  static String forgotPassword = '/user/forgot-password';
-  static String resetPassword = '/user/reset-password';
-  static String getReferralCode = '/user/get-my-referral-code';
-  static String verifyReferralCode = '/user/verify-admin-referral/';
-  static String subList = '/subscription/get';
-  static String slider = '/settings/get-sliders';
-  static String walletBalance = '/user/get-vendor-wallet-balance';
+
 
   // Products
   static String fetchAllProducts = '/product/get-products/';
   static String fetchAProduct = '/product/details/';
-
-  // static String fetchAProduct = '/product/get-product-details/';
-  static String fetchFlashSalesProduct = '/product/inventory/flashsales';
 
   // Reviews and Rating
   static String fetchAllReviews = '/product-reviews/reviews/';
   static String fetchAllRatings = '/product-reviews/ratings/';
   static String addReviews = '/product-reviews/reviews/';
 
-  // Cart
-  static String addToCart = "/cart/retailer/add";
-  static String viewCart = "/cart/retailer";
-  static String removeFromCart = "/cart/retailer/remove/";
-  static String clearCart = "/cart/retailer/clear";
-  static String cartEstimatedPrice = "/cart/est-delivery-price";
-
-  // Cart
-  static String order = "/orders/new-order";
-  static String orderHistory = "/orders/my-orders";
-  static String orderDetails = "/orders/";
-  static String voucherDetails = "/vouchers/value/";
-  static String cancelOrder = "/orders/";
-
-  // Payment
-  static String initiatePayment = '/payments/initiate-payment';
-
 // Favourite
   static String addToFavourite = "/orders/add-to-favs/";
   static String viewFavourite = "/orders/favorites";
   static String removeFromFavourite = "/orders/remove-from-favs/";
-
-  // Categories
-  static String getCategories = "/category/";
-
-  // PickUp Location
-  static String getAllPickupLocation = "/settings/add-pickup-location/";
-  static String getPickupLocationByLGA = "/settings/get-picklocation-by-lga/";
-
-  // AddUserLocation
-  static String addUserAddress = "/user/add-address";
-  static String getUserAddress = "/user/get-addresses";
-  static String removeUserAddress = "/user/remove-address";
-  static String updateActiveAddress = '/user/update-active-address/';
-
-  // Search product
-  static String searchProductByCategory = "/product/get-products-by-category/";
-  static String searchProductByName = "/product/search-products/";
-
-  // Notification
-  static String getAllNotification = "/account/my-notifications";
-  static String sendSinglePushNotification = "/account/notification";
 
   // receiveTimeout
   static const Duration receiveTimeout = Duration(milliseconds: 60000);
--- lib/network/api/user/api_rep.dart
+++ lib/network/api/user/api_rep.dart
@@ -41,94 +41,6 @@ class ApiRepo {
     return response;
   }
 
-  Future<Response> updateTokenApi({
-    required String token,
-  }) async {
-    try {
-      final Response response = await dioClient.post(
-        ApiConstants.updateToken,
-        data: {
-          "token": token,
-        },
-      );
-      return response;
-    } catch (e) {
-      rethrow;
-    }
-  }
-
-  Future<Response> verifyAccountApi({
-    required String otp,
-  }) async {
-    try {
-      final Response response = await dioClient.put(
-        ApiConstants.verifyAccount,
-        data: {
-          "otp": otp,
-        },
-      );
-      return response;
-    } catch (e) {
-      rethrow;
-    }
-  }
-
-  Future<Response> forgotPasswordApi({
-    required String email,
-  }) async {
-    try {
-      final Response response = await dioClient
-          .put(ApiConstants.forgotPassword, data: {"email": email});
-      return response;
-    } catch (e) {
-      rethrow;
-    }
-  }
-
-  Future<Response> resetPasswordApi({
-    required String otp,
-    required String newPassword,
-  }) async {
-    try {
-      final Response response = await dioClient.put(ApiConstants.resetPassword,
-          data: {"otp": otp, "newPassword": newPassword});
-      return response;
-    } catch (e) {
-      rethrow;
-    }
-  }
-
-  Future<Response> getReferralCodeApi() async {
-    final Response response = await dioClient.get(
-      ApiConstants.getReferralCode,
-    );
-    return response;
-  }
-
-  Future<Response> verifyReferralCodeApi({
-    required String code,
-  }) async {
-    try {
-      final Response response = await dioClient.get(
-        ApiConstants.verifyReferralCode + code,
-      );
-      return response;
-    } catch (e) {
-      rethrow;
-    }
-  }
-
-  Future<Response> resendOTPApi() async {
-    try {
-      final Response response = await dioClient.get(
-        ApiConstants.resendOTP,
-      );
-      return response;
-    } catch (e) {
-      rethrow;
-    }
-  }
-
   Future<Response> loginUserApi(String? email, String? password) async {
     try {
       final Response response = await dioClient.post(
@@ -141,90 +53,24 @@ class ApiRepo {
     }
   }
 
-  Future<Response> updateUserProfileApi({
-    required String? firstName,
-    required String? lastName,
-    required String? gender,
-    required DateTime? dob,
-  }) async {
-    try {
-      final Response response = await dioClient.put(
-        ApiConstants.updateProfile,
-        data: {
-          "newDetails": {
-            "firstName": firstName ?? "",
-            "lastName": lastName ?? "",
-            "gender": gender ?? "", // male or female
-            "dob": "${dob ?? ""}"
-          }
-        },
-      );
-      return response;
-    } catch (e) {
-      rethrow;
-    }
-  }
-
-  Future<Response> updateUserPasswordApi({
-    required String? oldPassword,
-    required String? newPassword,
-  }) async {
-    try {
-      final Response response = await dioClient.put(
-        ApiConstants.updatePassword,
-        data: {"oldPassword": oldPassword, "newPassword": newPassword},
-      );
-      return response;
-    } catch (e) {
-      rethrow;
-    }
-  }
-
-  Future<Response> getUserProfileApi() async {
-    final Response response = await dioClient.get(ApiConstants.profile);
-    return response;
-  }
-
-  Future<Response> getSlidersApi() async {
-    final Response response = await dioClient.get(ApiConstants.slider);
-    return response;
-  }
-
-  Future<Response> getWalletBalanceApi() async {
-    final Response response = await dioClient.get(ApiConstants.walletBalance);
-    return response;
-  }
-
-  Future<Response> fetchAllProductApi(
-      {required String pageNumber, required String limit}) async {
-    final Response response = await dioClient
-        .get("${ApiConstants.fetchAllProducts}$pageNumber/$limit");
-    return response;
-  }
-
-  Future<Response> fetchFlashSalesProductApi(
-      {required String pageNumber, required String limit}) async {
-    final Response response = await dioClient
-        .get("${ApiConstants.fetchFlashSalesProduct}/$pageNumber/$limit");
+  Future<Response> fetchAllProductApi({required String pageNumber, required String limit}) async {
+    final Response response = await dioClient.get("${ApiConstants.fetchAllProducts}$pageNumber/$limit");
     return response;
   }
 
   Future<Response> fetchAProductApi(String id) async {
     // print("${ApiConstants.fetchAProduct}$id");
-    final Response response =
-        await dioClient.get("${ApiConstants.fetchAProduct}$id");
+    final Response response = await dioClient.get("${ApiConstants.fetchAProduct}$id");
     return response;
   }
 
   Future<Response> fetchReviewsApi(String id) async {
-    final Response response =
-        await dioClient.get("${ApiConstants.fetchAllReviews}$id");
+    final Response response = await dioClient.get("${ApiConstants.fetchAllReviews}$id");
     return response;
   }
 
   Future<Response> fetchRatingsApi(String id) async {
-    final Response response =
-        await dioClient.get("${ApiConstants.fetchAllRatings}$id");
+    final Response response = await dioClient.get("${ApiConstants.fetchAllRatings}$id");
     return response;
   }
 
@@ -234,67 +80,10 @@ class ApiRepo {
     required String comment,
     required int rating,
   }) async {
-    final Response response = await dioClient.post(
-        "${ApiConstants.addReviews}$id",
-        data: {"title": title, "comment": comment, "rating": rating});
+    final Response response = await dioClient
+        .post("${ApiConstants.addReviews}$id", data: {"title": title, "comment": comment, "rating": rating});
     return response;
   }
-
-  Future<Response> estimatedPriceApi() async {
-    try {
-      final response = await dioClient.get(ApiConstants.cartEstimatedPrice);
-      return response;
-    } catch (e) {
-      rethrow;
-    }
-  }
-
-  /*------------------ Cart Api Call Start----------------*/
-
-  Future<Response> addToCartApi(CartItem cartItem) async {
-    try {
-      final Response response = await dioClient.post(
-        ApiConstants.addToCart,
-        data: cartItem.toJson(),
-      );
-      return response;
-    } catch (e) {
-      rethrow;
-    }
-  }
-
-  Future<Response> fetchCartApi() async {
-    try {
-      final Response response = await dioClient.get(
-        ApiConstants.viewCart,
-      );
-      return response;
-    } catch (e) {
-      rethrow;
-    }
-  }
-
-  Future removeCartItemApi(String id) async {
-    try {
-      final response = await dioClient.delete(
-        ApiConstants.removeFromCart + id,
-      );
-      return response;
-    } catch (e) {
-      rethrow;
-    }
-  }
-
-  Future clearCartItemsApi() async {
-    try {
-      final response = await dioClient.delete(ApiConstants.clearCart);
-      return response;
-    } catch (e) {
-      rethrow;
-    }
-  }
-
-  /*------------------ Cart Api Call End----------------*/
 
   /*------------------ Favourite Api Call Start----------------*/
 
@@ -332,195 +121,4 @@ class ApiRepo {
   }
 
 /*------------------ Favourite Api Call End----------------*/
-
-  Future<Response> fetchCategoriesApi(
-      {required String pageNumber, required String limit}) async {
-    try {
-      final Response response = await dioClient.get(
-        "${ApiConstants.getCategories}$pageNumber/$limit",
-      );
-      return response;
-    } catch (e) {
-      rethrow;
-    }
-  }
-
-  Future<Response> fetchCategoryProductsApi(
-      {required String page,
-      required String categoryID,
-      required String limit}) async {
-    try {
-      final Response response = await dioClient.get(
-        "${ApiConstants.searchProductByCategory}$categoryID/$page/$limit",
-      );
-      return response;
-    } catch (e) {
-      rethrow;
-    }
-  }
-
-  Future<Response> fetchSearchedProductsApi(
-      {required String page,
-      required String name,
-      required String limit}) async {
-    try {
-      final Response response = await dioClient.get(
-        "${ApiConstants.searchProductByName}$name/$page/$limit",
-      );
-      return response;
-    } catch (e) {
-      rethrow;
-    }
-  }
-
-  Future<Response> addUserAddressApi({
-    required String firstname,
-    required String lastname,
-    required String street,
-    required String state,
-    required String lga,
-    required String city,
-    required String direction,
-    required String phoneNumber,
-  }) async {
-    try {
-      final Response response =
-          await dioClient.put(ApiConstants.addUserAddress, data: {
-        "firstName": firstname,
-        "lastName": lastname,
-        "streetAdd": street,
-        "state": state,
-        "localGovt": lga,
-        "city": city,
-        "direction": direction,
-        "phoneNumber": phoneNumber
-      });
-      return response;
-    } catch (e) {
-      rethrow;
-    }
-  }
-
-  Future<Response> fetchUserAddressesApi() async {
-    try {
-      final Response response = await dioClient.get(
-        ApiConstants.getUserAddress,
-      );
-      return response;
-    } catch (e) {
-      rethrow;
-    }
-  }
-
-  Future<Response> updateActiveAddressesApi(String addressID) async {
-    try {
-      final Response response = await dioClient.put(
-        ApiConstants.updateActiveAddress + addressID,
-      );
-      return response;
-    } catch (e) {
-      rethrow;
-    }
-  }
-
-  Future<Response> fetchLocationsByLGAApi(String lga) async {
-    // print("${ApiConstants.getPickupLocationByLGA}$lga");
-    final Response response =
-        await dioClient.get("${ApiConstants.getPickupLocationByLGA}$lga");
-    return response;
-  }
-
-  Future<Response> removeUserAddressesApi(String id) async {
-    try {
-      final Response response = await dioClient.delete(
-        "${ApiConstants.removeUserAddress}/$id",
-      );
-      return response;
-    } catch (e) {
-      rethrow;
-    }
-  }
-
-  /*------------------ Order Api Call Start----------------*/
-
-  Future<Response> newOrderApi(OrderData order) async {
-    try {
-      final Response response = await dioClient.post(
-        ApiConstants.order,
-        data: order.toJson(),
-      );
-      return response;
-    } catch (e) {
-      rethrow;
-    }
-  }
-
-  Future<Response> fetchOrderHistoryApi() async {
-    try {
-      final Response response = await dioClient.get(
-        ApiConstants.orderHistory,
-      );
-      return response;
-    } catch (e) {
-      rethrow;
-    }
-  }
-
-  Future<Response> cancelOrderApi(String orderID) async {
-    try {
-      final Response response = await dioClient.put(
-          "${ApiConstants.cancelOrder}$orderID/status/user",
-          data: {"status": "cancelled"});
-      return response;
-    } catch (e) {
-      rethrow;
-    }
-  }
-
-  Future<Response> fetchOrderDetailsApi(String id) async {
-    try {
-      final Response response = await dioClient.get(
-        ApiConstants.orderDetails + id,
-      );
-      return response;
-    } catch (e) {
-      rethrow;
-    }
-  }
-
-  Future<Response> fetchVoucherDetailsApi(String code) async {
-    try {
-      final Response response = await dioClient.get(
-        ApiConstants.voucherDetails + code,
-      );
-      return response;
-    } catch (e) {
-      rethrow;
-    }
-  }
-
-/*------------------ Order Api Call End----------------*/
-
-  Future<Response> paymentApi({
-    required String? paymentMethod,
-    required String? deliveryOption,
-    required String? voucher,
-  }) async {
-    try {
-      final Response response = await dioClient.post(
-        ApiConstants.initiatePayment,
-        data: {
-          // "orderId": orderID,
-          // "currency": "NGN"
-          "currency": "NGN",
-          "voucher": voucher ?? "",
-          "paymentMethod": paymentMethod ?? "cash", // cash or online
-          "deliveryOption": deliveryOption ?? "pickup" // pickup or door
-        },
-      );
-      return response;
-    } catch (e) {
-      rethrow;
-    }
-  }
 }
